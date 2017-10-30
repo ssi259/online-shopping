1638fea2a58b00f6d3d635df6cfcc229b868e681
shivendra singh <shivendraps230@gmail.com>
2017-10-30 20:56:15 +0530

    Static resources

--- onlineshopping/src/main/java/net/km/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/km/onlineshopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package net.km.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,6 +16,30 @@ public class PageController {
 		mv.addObject("greeting","Welcome to spring");
 		return mv;
 	}
+	@RequestMapping(value={"/test"})
+	public ModelAndView test(@RequestParam(value="greet",required=false)String greeting )
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}
+	
+	@RequestMapping(value={"/test/{greet}"})
+	public ModelAndView test1(@PathVariable("greet")String greet)
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting",greet);
+		return mv;
+		
+	}
+//	@RequestMapping(value={"/test/{greet}"})
+//	public ModelAndView test1(@PathVariable("greet")String greet )
+//	{
+//		ModelAndView mv=new ModelAndView("page");
+//		mv.addObject("greeting",greet);
+//		return mv;
+//	}
+	
 	
 
 }
